6c0bb4f9586e0f83cb1b36db0232899260c24028
Ruben <ruben_information@yahoo.ro>
2020-04-30 17:50:54 +0300

    Solve conflicts

--- src/config.js
+++ src/config.js
@@ -1,28 +1,27 @@
-const fs = require('fs')
+const fs = require("fs");
 
 // PRIVATE and PUBLIC Key
-const privateKEY = fs.readFileSync('./private.key', 'utf8')
-const publicKEY = fs.readFileSync('./public.key', 'utf8')
+const privateKEY = fs.readFileSync("./private.key", "utf8");
+const publicKEY = fs.readFileSync("./public.key", "utf8");
 
-let i = 'Authentication API'           // Issuer
-let s = 'some@user.com'         // Subject 
-let a = 'User_Identity' // Audience
+let i = "Authentication API"; // Issuer
+let s = "some@user.com"; // Subject
+let a = "User_Identity"; // Audience
 // SIGNING OPTIONS
 const signOptions = {
   issuer: i,
   subject: s,
   audience: a,
-  expiresIn: '12h',
-  algorithm: 'RS256'
-}
-
+  expiresIn: "12h",
+  algorithm: "RS256"
+};
 
 module.exports = {
   secret: {
-    sample: '$_R4I.u1l.B2C.e7i.UNc_$',
+    sample: "$_R4I.u1l.B2C.e7i.UNc_$",
     privateKEY,
     publicKEY,
     signOptions
   },
-  mongoURI: 'mongodb://localhost:27017'
-}
+  mongoURI: "mongodb+srv://GrapeKode:R4u1B2e7N@blog-bdste.mongodb.net"
+};
